a5348da440d5c4ae2a97f3428afcb4673db4de22
Naume-k <kobnaome@gmail.com>
2019-09-29 20:32:30 +0200

    secret key

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SECRET_KEY ='a random variable1'
+export SECRET_KEY ='some_value'
 export MAIL_USERNAME='kobnaome@gmail.com'
 export MAIL_PASSWORD='naome4christ@1994'
     python3.6 manage.py server

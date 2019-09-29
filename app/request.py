688262f9d26edfb470f658df3965d82becc9da16
Naume-k <kobnaome@gmail.com>
2019-09-29 19:39:58 +0200

    added the codes to request for the quotes

---
+++ app/request.py
@@ -0,0 +1,23 @@
+import urllib.request,json
+from .models import Quotes
+import requests
+from config import Config
+
+base_url = Config.QUOTE_URL
+# def configure_request(app):
+#     global base_url
+#     base_url = app.config["QUOTE_URL"]
+
+def get_quotes():
+    '''
+    Function that gets the json response to our url request
+    '''
+    quote_object = requests.get(base_url)
+    new_quote=quote_object.json()
+    quote = new_quote.get('quote')
+    author = new_quote.get('author')
+    quote_object = Quotes(quote,author)
+        
+
+    return quote_object
+    
